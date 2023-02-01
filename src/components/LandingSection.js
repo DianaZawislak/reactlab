7b0c191c7f69e8d9ab2108c9520fba722f91c9b1
Diana Zawislak <diana.zawislak@icloud.com>
2023-02-01 13:28:04 -0500

    added components in landing secton and colors

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import { Avatar, Box, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
+import avatar from "../images/generic-avatar.png";
 
 const greeting = "Hello, I am Diana!";
 const bio1 = "A frontend developer";
@@ -8,15 +9,28 @@ const bio2 = "Specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
+
+
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
     alignItems="center"
     isDarkBackground
-    backgroundColor="#808080"
+    bgGradient='linear(to-r, #C0C0C0, blue.400, #C0C0C0)'
   >
+  <VStack>
+    <VStack mb="8">
+      <Avatar size='2xl' src={avatar}/>
+      <profile size='60'>{greeting}</profile>
+    </VStack>
+      <Heading>{bio1}</Heading>
+      <Heading>{bio2}</Heading>
+  </VStack>
 
   </FullScreenSection>
+
+
+
 );
 
 export default LandingSection;
