5edbffba3b56927568b6b6d9df06dc52e27f395d
Diana Zawislak <diana.zawislak@icloud.com>
2023-02-04 16:05:48 -0500

    acrds styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,10 +10,10 @@ const Card = ({ title, description, imageSrc }) => {
 
 
  return (
-    <VStack align="left" bg="white" color="black" p="3" borderRadius="12px">
+    <VStack align="left" bg="grey" color="white" p="3" borderRadius="12px">
       <Image src={imageSrc} alt={title} />
-      <Heading size='sm'>{title}</Heading>
-      <Text>{description}</Text>
+      <Heading size='lg'>{title}</Heading>
+      <Text fontSize={20}>{description}</Text>
       <HStack align="center" justify="left">
         <Text>See more</Text>
         <FontAwesomeIcon icon={faArrowRight} size="1x" />
